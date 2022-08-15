6d76f263b8e738a525533b859666ccfddc44db51
RozkeAdine <rozke-adine.bouyahia-std@donkey.school>
2022-08-15 11:34:52 +0200

    starting challenge 02

---
+++ ChallengeAngular02/src/app/block-component/block-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-block-component',
+  templateUrl: './block-component.component.html',
+  styleUrls: ['./block-component.component.sass']
+})
+export class BlockComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
